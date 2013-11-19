f221f43e4146ceebd6e3d62b62fe3d79a7338a47
Wojciech Syta <wojciech.syta@stonybrook.edu>
2013-11-19 01:36:14 -0500

    v .3

--- GoBoard.lisp
+++ GoBoard.lisp
@@ -25,50 +25,63 @@
 			     nil)
 			    (t
 			     (setC x y wORb lib)
-			     (propogateLibChanges x x wORb lib))))))))
+			     (checkSurr x y wORb 3 lib))))))))
 
-
-(defun checkSurr (x y wORb &optional (checking 1))
+(defun checkSurr (x y wORb &optional (checking 1)(lib 0))
 	   (let*
 	    ((count 0)
 	     (lt (genSurr x y))
-	     (dirc '(left right down up))
+	     (left (list (- x 1) y))
+	     (right (list (+ x 1) y))
+	     (down (list x (- y 1)))
+	     (up (list x (+ y 1)))
+	     (dirc (list left right down up))
 	     (temp '()))
 	     
 					;Modes checking or setting or destroying
 					;2 == Destroying
 	     (cond ((= checking 2)
 		    (loop for i in lt do
-			 (cond ((equal (car i) wORb)
-				(destroy (car (car dirc)) wORb)))
+			 (cond ((and (not (equal i '(nil nil)))
+				     (equal (car i) wORb))		       
+				(destroy (car  dirc) wORb)))
 			 (setf dirc (cdr dirc))))
-	     	   
 		   
-					;1 == checking
+					;propogating Liberty changes		   
+		   ((= checking 3)
+		    (loop for i in lt do
+			 (cond ((and (not (equal i '(nil nil)))
+				     (equal (car dirc) wORb)
+				     (not (equal (second (car dirc)) lib)))
+				(checkSurr (car i)(second i) wORb 3 lib)))
+			 (setf dirc (cdr dirc))))
+		    		    		      	   
+					;calculating Liberties
 		   ((= checking 1)
-					
 		    (setf temp dirc)
 					;if any adjacent are empty
 		    (loop for i in lt do
-			 (cond ((= (last i) 999)
+			 (cond ((and (not (equal i '(nil nil)))
+				     (= (second i) 999))
 				(incf count)))
 			 (setf temp (cdr temp)))
 		    			
 		    (setf temp dirc)
 					;if any adjacent are same color
 		    (loop for i in lt do
-			 (cond ((equal (car i) wORb)
-				(setf count (+ count (last i)))))
+			 (cond ((and (not (equal i '(nil nil)))
+				     (equal (car i) wORb))
+				(setf count (+ count (second i)))))
 			 (setf temp (cdr temp)))
-				       
+
 					;if any opposite color, with only 1 Liberty
-		    (loop for i in lt do
-			 (cond ((and (not (= (car i) wORb))
-				     (= 1 (last i)))
+		    (loop for i in lt do 
+			 (cond ((and (not (equal i '(nil nil)))
+				     (not (equal (car i) wORb)))
+				(= 1 (second i))
 				(incf count)
-				(destroy (car (car dirc)) wORb)))
+				(destroy (car dirc) wORb)))
 			 (setf dirc (cdr dirc)))
-		   		
 		    count))))
 
 (defun destroy (lst wORb)
@@ -77,40 +90,9 @@
 	     (setC x y 'N 999)
 	     (checkSurr x y wORb 2)))
 
-(defun propogateLibChanges (x y wORb lib)
-	   (let*(
-		(a (filled (- x 1) y)) 
-		(b (filled (+ x 1) y)) 
-		(c (filled x (+ y 1))) 
-		(d (filled x (- y 1))) 
-		(leftColor (car a))
-		(leftLibs (car (cdr a)))
-		(rightColor (car b))
-		(rightLibs (car (cdr b)))
-		(upColor (car c))
-		(upLibs (car (cdr c)))
-		(downColor (car d))
-		(downLibs (car (cdr d))))
-	     (cond ((and (not (equal rightLibs lib))
-			 (equal rightColor wORb))
-		    (setC (+ x 1) y wORb lib)
-		    (propogateLibChanges (+ x 1) y wORb lib)))
-	     (cond ((and (not (equal leftLibs lib))
-			 (equal leftColor wORb))
-		    (setC (- x 1) y wORb lib)
-		    (propogateLibChanges (- x 1) y wORb lib)))
-	     (cond ((and (not (equal upLibs lib))
-			 (equal upColor wORb))
-		    (setC x (+ y 1) wORb lib)
-		    (propogateLibChanges x (+ y 1) wORb lib)))		   
-	     (cond ((and (not (equal downLibs lib))
-			 (equal downColor wORb))
-		    (setC x (- y 1) wORb lib)
-		    (propogateLibChanges x (- y 1) wORb lib)))))
-
 (defun genSurr (x y)
 	   (list
-	    (cond ((> x 1)
+	    (cond ((> x 0)
 		   (let* (
 			  (a (filled (- x 1) y))
 			  (leftColor (car a))
@@ -118,7 +100,7 @@
 		     (list leftColor leftLibs)))
 		  (t
 		   '(NIL NIL)))
-	    (cond ((< x 19)
+	    (cond ((< x 8)
 		   (let* (				 
 			  (b (filled (+ x 1) y)) 
 			  (rightColor (car b))
@@ -126,7 +108,7 @@
 		     (list rightColor rightLibs)))
 		  (t
 		   '(NIL NIL)))
-	    (cond ((> y 1)
+	    (cond ((> y 0)
 		   (let* (
 			  (d (filled x (- y 1)))
 			  (downColor (car d))
@@ -134,7 +116,7 @@
 		     (list downColor downLibs)))
 		  (t
 		   '(NIL NIL)))
-	    (cond ((< y 19)				    
+	    (cond ((< y 8)				    
 		   (let* (
 			  (c (filled x (+ y 1))) 
 			  (upColor (car c))
